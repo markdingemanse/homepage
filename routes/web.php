efb5f17105ded6e40778e2fafabf56cabd89c5f1
Mark Dingemanse <dingemansemark@gmail.com>
2020-12-01 23:00:34 +0100

    removed unused feature

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,3 @@ Route::get('/', 'Homepage\HomepageController@launch')->name('homepage');
 Route::get('randomfile', 'Homepage\HomepageController@getRandomFile')->name('getFile');
 Route::get('uploadfile','Homepage\HomepageController@uploadfileview')->name('upload_background_view');
 Route::post('uploadfile','Homepage\HomepageController@uploadFile')->name('upload_background_post');
-
-Route::get('titan', 'Titan\TitanController@index')->name('titan_index');
-Route::post('titan/log', 'Titan\TitanController@storeLog')->name('upload_log');
-Route::post('titan/heroine', 'Titan\TitanController@storeHeroine')->name('upload_log');
